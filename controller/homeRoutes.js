64c182d395159a836c72142866210d2e1f08d9f0
hanszilzer <hanszilzer@gmail.com>
2023-12-12 19:49:58 -0600

    added profile route

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,8 +1,51 @@
 const router = require('express').Router();
-// const { Project, User } = require('../models');
+const { Filament, Material, Printer, Settings, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 router.get('/login', (req, res) => {
