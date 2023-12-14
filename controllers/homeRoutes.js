7de89ad8c9bcb949b7b2addebbfd8118b11f0e44
hanszilzer <hanszilzer@gmail.com>
2023-12-13 18:19:03 -0600

    fixed homepage route variables

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,7 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const printerData = await Printer.findAll({
       include: [
         {
           model: User,
@@ -16,11 +16,11 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const printers = printerData.map((printer) => printer.get({ printer: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      printers, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
