f73d6b8693e71c6f1e6ac32f72505d67dbfbb75f
hanszilzer <hanszilzer@gmail.com>
2023-12-18 21:20:57 -0600

    added an annoying console log

--- controllers/api/settingRoutes.js
+++ controllers/api/settingRoutes.js
@@ -6,19 +6,24 @@ router.get('/', withAuth, async (req, res) => {
   try {
    
     const allSettings = await Settings.findAll({
-      include: [{
-        model: Printer,
-        as: 'printer',
-        include: [{
-          model: Filament,
-          as: 'filament',
-        }]
-      }]
+      where:{
+        userId: req.session.user_id
+      },
+      include:[Printer, Filament]
+      //   include: [{
+      //   model: Printer,
+      //   as: 'printer',
+      //   include: [{
+      //     model: Filament,
+      //     as: 'filament',
+      //   }]
+      // }]
     });
 
     const settings = allSettings.map((setting) => setting.get({ setting: true }));
-
+console.log(settings, "this is line 24");
     res.render('settings', { settings });
+    
   } catch (error) {
     console.error('Error fetching settings:', error);
     res.status(500).send('Internal Server Error');
